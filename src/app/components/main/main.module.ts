235ce5741ae559f8a177cc9d3c2e1508a6a5fa78
Lora Ruffin <lora.ruffin@promontech.com>
2018-12-31 11:10:33 -0700

    SB5: layout

--- src/app/components/main/main.module.ts
+++ src/app/components/main/main.module.ts
@@ -1,13 +1,17 @@
 
 import { MainRoutingModule } from './main-routing.module';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule, MatToolbarModule } from '@angular/material';
+
 
 import { MainComponent } from './main.component';
 
 
 @NgModule({
     imports: [
-        MainRoutingModule
+        MainRoutingModule,
+        MatSidenavModule,
+        MatToolbarModule
     ],
     declarations: [
         MainComponent
